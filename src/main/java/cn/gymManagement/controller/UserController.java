1c04fcf76bed4bbd5884b170e6cb4544363c8201
xyd <597241326@qq.com>
2019-04-30 16:29:48 +0800

    第十四次上传，第四次优化，拓展优化。

--- src/main/java/cn/gymManagement/controller/UserController.java
+++ src/main/java/cn/gymManagement/controller/UserController.java
@@ -306,25 +306,28 @@ public class UserController {
 
     /**
      * 会员修改密码 依据身份证号
+     *
      * @param identityCard 身份证号码
-     * @param newPassword 新密码
+     * @param newPassword  新密码
      * @return
      */
-    @RequestMapping(value = "updatePwd",method = RequestMethod.PUT)
+    @RequestMapping(value = "updatePwd", method = RequestMethod.POST)
     @ResponseBody
-    public int updatePwd(String identityCard,String newPassword){
+    public int updatePwd(String identityCard, String newPassword) {
+        System.out.println("获取：" + identityCard + ":" + newPassword);
         //判断是否存在该会员
-        User userIdCards = userService.getUserIdCard(identityCard);
-        if (userIdCards==null){
+        User users=userService.getUserIdCard(identityCard);
+        System.out.println("会员信息：" + users);
+        if (users == null) {
             System.out.println("不存在该会员");
             return 2;
-        }else {
+        } else {
             //开始修改
-            int row=userService.updatePwd(identityCard, newPassword);
-            if (row==1){
+            int row = userService.updatePwd(newPassword,identityCard);
+            if (row == 1) {
                 System.out.println("修改成功");
                 return 0;
-            }else {
+            } else {
                 System.out.println("修改失败");
                 return 1;
             }
